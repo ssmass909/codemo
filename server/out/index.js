1a45b3489e2e8df399d54029be130cc29d17e3bf
Saba Samkharadze <samkharadze.saba01@gmail.com>
2025-05-23 16:15:56 +0400

    testing precommit hook

--- server/out/index.js
+++ server/out/index.js
@@ -1,13 +1,10 @@
 import express from "express";
 const app = express();
 const port = 3000;
-
 app.get("/", (req, res) => {
-  res.json({ message: "Hello, World!" });
+    res.json({ message: "Hello, World!" });
 });
-
 app.listen(port, () => {
-  console.log(`express app is running on port ${port}`);
+    console.log(`express app is running on port ${port}`);
 });
-
 console.log("this change was made to test git precommit hook");
