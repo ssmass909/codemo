d7c3ccc7b649c82be38d4d250b15ef38676727ef
Saba Samkharadze <samkharadze.saba01@gmail.com>
2025-05-23 16:12:55 +0400

    first commit; configured dev environments

---
+++ client/src/App.tsx
@@ -0,0 +1,4 @@
+const App = () => {
+  return <div>hello world</div>;
+};
+export default App;
---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.listen(port, () => {
+  console.log(`express app is running on port ${port}`);
+});
